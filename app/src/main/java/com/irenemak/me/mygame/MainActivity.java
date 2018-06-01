1b5c3828a9dd5d79a62f4888577303c6bf94e073
Irene Mak <irenemak@Irenes-MBP.Home>
2018-05-31 22:51:20 -0700

    Initial commit

---
+++ app/src/main/java/com/irenemak/me/mygame/MainActivity.java
@@ -0,0 +1,16 @@
+package com.irenemak.me.mygame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManger.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+    }
+}
